8dc8e5ad2a4f0a5cf32e3f9b2dfebaae89de7d31
sunxiaodongme <hys_521@126.com>
2016-03-10 13:16:58 +0800

    加载图片导致内存泄漏，更能分析问题
    加载图片导致内存泄漏，更能分析问题

--- app/src/main/java/com/example/sunxiaodong/memleakanalyse/StaticReferenceActivity.java
+++ app/src/main/java/com/example/sunxiaodong/memleakanalyse/StaticReferenceActivity.java
@@ -1,9 +1,12 @@
 package com.example.sunxiaodong.memleakanalyse;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 
 /**
  * Created by sunxiaodong on 2016/1/27.
@@ -14,12 +17,22 @@ public class StaticReferenceActivity extends AppCompatActivity {
     private static final String TAG = "sxd";
 
     public static StaticReferenceActivity sStaticReferenceActivity;
+    private static Bitmap sBitmap;
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_static_reference);
         sStaticReferenceActivity = this;
+        initView();
+    }
+
+    private void initView() {
+        mImageView = (ImageView) this.findViewById(R.id.image_view);
+        sBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.image);
+        mImageView.setImageBitmap(sBitmap);
     }
 
     @Override
